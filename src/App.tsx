c72418e2e73ecb2e9a082961c7c1188466388478
go2023 <tetyanago2018@gmail.com>
2024-08-13 19:03:54 -0700

    App.tsx was adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useMultistepForm } from "./useMultistepForm.ts";
 
 function App() {
 
-  return <h1>Hi</h1>
+    const { steps, currentStepIndex} = useMultistepForm([
+        <div>One</div>,
+        <div>Two</div>,
+    ])
+
+
+    return (
+      <div
+          style={{
+              position: "relative",
+              background: "white",
+              border: "1px solid black",
+              padding: "2rem",
+              margin: "1rem",
+              borderRadius: ".5rem",
+              fontFamily: "Arial",
+              maxWidth: "max-content",
+          }}
+      >
+    <form>
+        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+            {currentStepIndex + 1} / {steps.length}
+        </div>
+    </form>
+  </div>)
 }
 
 export default App
